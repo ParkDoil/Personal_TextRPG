c0fc05d40ebf94bf906d8918817b28a2e1e960ad
Park Doil <poker5323@naver.com>
2022-06-17 17:51:31 +0900

    [Make] Text RPG base 텍스트RPG 기틀

---
+++ Text_RPG/Text_RPG/Player.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Common.h"
+
+class Player
+{
+public:
+	void SettingStatus()
+	{
+
+	}
+private:
+	int		_health = 0;
+	int		_maxHP = 0;
+	int		_stamina = 0;
+	int		_maxStamina = 100;
+	int		_power = 0;
+	int		_defense = 0;
+	int		_level = 0;
+};
---
+++ Text_RPG/Text_RPG/main.cpp
@@ -0,0 +1,13 @@
+#include "Common.h"
+
+#include "Battle.h"
+#include "Shop.h"
+#include "Monster.h"
+#include "Player.h"
+
+int main()
+{
+
+
+	return 0;
+}
